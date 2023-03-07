7ddf99475ad8027b4dac5078480a0ccccdd22692
Leahnicoles <lnsimonn@hotmail.com>
2023-03-07 16:03:24 +0000

    add dom variables and event listener to enter button

---
+++ code/lab_jsevents_start_code/script.js
@@ -0,0 +1,11 @@
+//DOM Variables
+const enterBtn = document.getElementById("enter");
+const textInput = document.getElementById("new-todo");
+const toDoList = document.getElementById("list");
+
+//Enter Button Add Event Listener.
+enterBtn.addEventListener("click", () => {
+    console.log("Button Clicked")
+    
+});
+
